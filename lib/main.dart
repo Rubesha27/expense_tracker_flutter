2b93119ac87a9ee6f874ea16895f46ec3d648330
Rubesha27 <rubesha27@gmail.com>
2024-10-12 22:47:04 +0500

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker_app/data/expense_data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'screens/home_screen.dart';
 
 void main() {
@@ -8,12 +10,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SmartExpense',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => ExpenseData(),
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,102 @@
+import 'package:expense_tracker_app/data/expense_data.dart';
+import 'package:expense_tracker_app/models/expense_item.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //Controllers
+  final TextEditingController newExpenseNameController =
+      TextEditingController();
+
+  final TextEditingController newExpenseAmountController =
+      TextEditingController();
+
+  //add new expense
+  void addNewExpense() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text("Add new expense"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  //expense name
+                  TextField(
+                    controller: newExpenseNameController,
+                  ),
+                  //expense amount
+                  TextField(
+                    controller: newExpenseAmountController,
+                  )
+                ],
+              ),
+              actions: [
+                //save button
+                MaterialButton(
+                  onPressed: save,
+                  child: const Text("Save"),
+                ),
+
+                //cancel button
+                MaterialButton(
+                  onPressed: cancel,
+                  child: const Text("Cancel"),
+                ),
+              ],
+            ));
+  }
+
+  //save
+  void save() {
+    //create expense item
+    ExpenseItem newExpense = ExpenseItem(
+        name: newExpenseNameController.text,
+        amount: newExpenseAmountController.text,
+        dateTime: DateTime.now());
+    Provider.of<ExpenseData>(context, listen: false).addNewExpense(newExpense);
+    Navigator.pop(context);
+  }
+
+  //cancel
+  void cancel() {
+    Navigator.pop(context);
+  }
+
+  //clear controllers
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    newExpenseAmountController.clear();
+    newExpenseNameController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('SmartExpense')),
-      body: Scaffold(),
-    );
+    return Consumer<ExpenseData>(
+        builder: (context, value, child) => Scaffold(
+              appBar: AppBar(title: const Text('SmartExpense')),
+              backgroundColor: Colors.green.shade300,
+              floatingActionButton: FloatingActionButton(
+                onPressed: addNewExpense,
+                child: const Icon(Icons.add),
+              ),
+              body: ListView.builder(
+                itemCount: value.getAllExpenseList().length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(value.getAllExpenseList()[index].name),
+                  subtitle: Text(
+                      "${value.getAllExpenseList()[index].dateTime.day} / ${value.getAllExpenseList()[index].dateTime.month} / ${value.getAllExpenseList()[index].dateTime.year}"),
+                  trailing:
+                      Text("Rs${value.getAllExpenseList()[index].amount}"),
+                ),
+              ),
+            ));
   }
 }
